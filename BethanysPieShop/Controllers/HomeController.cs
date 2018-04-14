8611827b9d497ac60f879efff764018c998d7585
melodie <josmelodie@gmail.com>
2018-04-14 13:57:24 +1200

    initial commit

---
+++ BethanysPieShop/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using BethanysPieShop.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BethanysPieShop.Controllers
+{
+    public class HomeController : Controller //controller always inheirits from base controller class
+    {
+        private readonly IPieRepository _pieRepository;
+
+        public HomeController(IPieRepository pieRepository) //dependancy injection will automatically inject an instance of the MockPieRepository here becuase of the Startup class, also known as constructor injection
+        {
+            _pieRepository = pieRepository;
+        }
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
